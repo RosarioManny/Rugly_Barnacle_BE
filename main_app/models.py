7a6ca9a53df98bfbb064994412d78722311cc89a
RosarioManny <rosario.emm47@gmail.com>
2025-08-01 19:02:15 -0400

    Create Models

--- main_app/models.py
+++ main_app/models.py
@@ -12,9 +12,23 @@ class Product(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
+  # Change the name of the display on admin panel
   def __str__(self):
     return self.name
+  
 # TODO:: Create Cart Model
+class Cart(models.Model):
+  session_key = models.CharField(max_length=48, unique=True)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
 # TODO:: Create CartItem Model
+class CartItem(models.Model):
+  cart = models.ForeignKey(Cart, related_name='items', on_delete=models.CASCADE)
+  product = models.ForeignKey(Product )
 # TODO:: Create CustomOrder Model
 # TODO:: Create Session Model
+# TODO:: Create Category Model
+
+"""
+"""
