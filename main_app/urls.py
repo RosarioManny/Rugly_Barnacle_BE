ea3b518fc240f5c2317e1b4a2ef67e129aba9e4f
RosarioManny <rosario.emm47@gmail.com>
2025-08-07 17:39:57 -0400

    Create ProductList, ProductDetails url+views

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
-from .views import Home
+from .views import *
 
 urlpatterns = [
-  path('', Home.as_view(), name='home')
+  path('', Home.as_view(), name='home'),
+  # TODO* :: Create products/ route
+  path('products/', ProductList.as_view(), name='product-list'),
+  # TODO :: Create products/<int:product_id> route
+  path('products/<int:id>', ProductDetails.as_view(), name='product-details')
+  # TODO :: Create cart/ route
+  # TODO :: Create cart/items route
+  # TODO :: Create cart/items/<int:item_id> route
+  # TODO :: Create categories/ route
+  # TODO :: Create custom/ route
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,29 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
+from .models import *
+from .serializers import *
 
 # Database Home view
 class Home(APIView):
   def get(self, request):
     content = {'Rugly Barncale: Welcome to the Rugly Barnacle Database!'}
     return Response(content)
+
+class ProductList(generics.ListCreateAPIView):
+  # Shows a list of all available products
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+  
+  # TODO: Create a method for filtering
+# TODO: Create ProductDetails 
+class ProductDetails(generics.RetrieveUpdateDestroyAPIView):
+  # Show the details of the specified single product
+  queryset = Product.objects.all()
+  serializer_class = ProductSerializer
+  lookup_field = 'id'
+# TODO: Create Cart 
+# TODO: Create CartDetails 
+# TODO: Create CustomOrder 
+# TODO: Create Category 
+# TODO: Create ProductList 
